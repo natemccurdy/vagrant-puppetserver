fec133dfc539953d1973d09b59616d229eda114a
Nate McCurdy <mccurdy.nate@gmail.com>
2019-05-09 10:25:57 -0700

    Add some docs, install r10k, set autosign

--- Puppetfile
+++ Puppetfile
@@ -2,9 +2,9 @@ forge 'https://forge.puppet.com'
 
 # Modules from the Puppet Forge
 # Versions should be updated to be the latest at the time you start
-#mod 'puppetlabs/inifile',     '2.2.1'
-#mod 'puppetlabs/stdlib',      '4.25.1'
-#mod 'puppetlabs/concat',      '4.2.1'
+mod 'puppetlabs/inifile',     '3.0.0'
+mod 'puppetlabs/stdlib',      '5.2.0'
+mod 'puppetlabs/concat',      '5.3.0'
 
 # Modules from Git
 # Examples: https://github.com/puppetlabs/r10k/blob/master/doc/puppetfile.mkd#examples
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,56 @@
 ## site.pp ##
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
+# This file (./manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
+# https://puppet.com/docs/puppet/latest/dirs_manifest.html
 #
 # Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
+# as should the default node definition if you want to use it.
 
 ## Active Configurations ##
 
 # Disable filebucket by default for all File resources:
-#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
+# https://github.com/puppetlabs/docs-archive/blob/master/pe/2015.3/release_notes.markdown#filebucket-resource-no-longer-created-by-default
 File { backup => false }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
+## Node Definitions ##
 
 # The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
+# definition. If there are no other node definitions in this file, classes
+# and resources declared in the default node definition will be included in
+# every node's catalog.
+#
+# Note that node definitions in this file are merged with node data from the
+# Puppet Enterprise console and External Node Classifiers (ENC's).
+#
+# For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+}
+
+
+## Vagrant Node Definitions ##
+#
+# These node definitions exist so you can experiment with your Puppet code in Vagrant.
+#
+#   * master.vagrant - For the most part, you should not change or add anything
+#                      to this. It's empty so that your Vagrant master stays neutral and your
+#                      Puppet code doesn't risk messing it up.
+#
+#   * agent.vagrant  - This is where you can add Puppet code to try out on the agent.
+#                      You can include a role, or a profile, or any arbitrary Puppet code you want.
+#
+node 'master.vagrant' {
+  # Intentionally left empty
+}
+
+# 1. Put the Puppet code you want to test in this node definition.
+# 2. SSH to the agent: vagrant ssh agent1
+# 3. Become root: sudo -s
+# 4. Do a Puppet run: puppet agent -t
+node /agent.*\.vagrant/ {
+
+
 }
